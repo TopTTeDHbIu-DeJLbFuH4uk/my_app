b3926c51ba83e072854096e7f3977a8d41f041a0
LaProfessional <118113982+LaProfessional@users.noreply.github.com>
2025-01-20 20:42:18 +0700

    React TEST 20.01.2025 20:42

---
+++ my_app/src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Counter from "./components/counter";
+// import ClassCounter from "./components/classCounter";
+
+const App = () => {
+
+    return (
+        <main className='main-container'>
+            <Counter/>
+            <Counter/>
+            <Counter/>
+
+            {/*<ClassCounter/>*/}
+            {/*<ClassCounter/>*/}
+            {/*<ClassCounter/>*/}
+
+
+        </main>
+    );
+};
+
+export default App;
