fba5ef93439463bafc4787f738cde95bea34d1fd
LaProfessional <118113982+LaProfessional@users.noreply.github.com>
2025-01-23 23:12:29 +0700

    23.01.2025 23:13

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 
 import PostList from "./components/UI/PostList";
 import PostForm from "./components/UI/PostForm";
@@ -9,6 +9,7 @@ import MyModal from "./components/UI/MyModal/MyModal";
 import MyButton from "./components/UI/button/MyButton";
 
 import {usePosts} from "./hooks/usePosts";
+import axios from "axios";
 
 const App = () => {
 
@@ -18,11 +19,19 @@ const App = () => {
     const [modal, setModal] = useState(false);
     const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
 
+    useEffect(() => {
+        fetchPosts();
+    }, [filter]);
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost]);
         setModal(false);
     };
+
+    const fetchPosts = async () => {
+        const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
+        setPosts(response.data);
+    }
 
     const removePost = post => {
         setPosts(posts.filter(p => p.id !== post.id))
@@ -31,10 +40,13 @@ const App = () => {
     return (
         <>
             <MyButton onClick={() => setModal(true)}>Create user</MyButton>
+
             <MyModal visible={modal} setVisible={setModal}>
                 <PostForm create={createPost}/>
             </MyModal>
+
             <PostFilter filter={filter} setFilter={setFilter}/>
+
             <PostList remove={removePost} posts={sortedAndSearchedPosts} title={'Posts about JS'}/>
         </>
     );
